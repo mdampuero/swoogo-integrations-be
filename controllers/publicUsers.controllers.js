94f91ad30c5af347b3dc835f085175399a5c1a44
Mauricio Ampuero <mdampuero@gmail.com>
2024-02-18 19:29:49 +0100

    Refactory

--- controllers/publicUsers.controllers.js
+++ controllers/publicUsers.controllers.js
@@ -1,15 +1,23 @@
 const { response, request } = require("express");
-const PublicUser = require("../models/publicUser");
 const axios = require('axios');
 const { authentication } = require("../helpers/swoogo-auth");
+const { parseErrorSwoogo, convertJson2Form, getString, deconstructAnswer, getAllFields } = require("../helpers/utils");
 
 const mapper = {
-    "external_id": "c_3803254",
-    "assistant_type": "c_3803393",
-    "country": "c_3803253",
+    "id"                 : "id",
+    "first_name"         : "first_name",
+    "last_name"          : "last_name",
+    "email"              : "email",
+    "mobile_phone"       : "mobile_phone",
+    "job"                : "job_title",
+    "company"            : "company",
+    "registration_status": "registration_status",
+    "external_id"        : "c_3803254",
+    "assistant_type"     : "c_3803393",
+    "country"            : "c_3803253",
     "company_description": "c_3803255",
-    "company_logo": "c_3803395",
-    "company_web": "c_3803396"
+    "company_logo"       : "c_3803395",
+    "company_web"        : "c_3803396"
 }
 
 const getAll = async (req = request, res = response) => {
@@ -18,7 +26,7 @@ const getAll = async (req = request, res = response) => {
     const instance = axios.create({
         baseURL: `${process.env.SWOOGO_APIURL}registrants.json`,
         params: {
-            fields: `id,first_name,last_name,email,mobile_phone,job_title,company,registration_status,${mapper.external_id},${mapper.assistant_type},${mapper.country},${mapper.company_description},${mapper.company_logo},${mapper.company_web}`,
+            fields: getString(mapper),
             'per-page': perPage,
             page,
             event_id: integration.event_id,
@@ -28,23 +36,24 @@ const getAll = async (req = request, res = response) => {
     });
     const resp = await instance.get();
     const { items, _meta } = resp.data;
+
     res.json({
         total: _meta.totalCount,
         pages: _meta.pageCount,
         limit: _meta.perPage,
-        items: items
+        items: getAllFields(items, mapper)
     });
 }
 
 const post = async (req, res = response) => {
     const body = req.body;
-    const formData = convertJson2Form(body);
+    const formData = convertJson2Form(body, mapper);
     formData.append('event_id', req.integration.event_id);
     try {
         const resp = await axios.post(`${process.env.SWOOGO_APIURL}registrants/create.json`, formData, {
             headers: { "Authorization": "Bearer " + await authentication() }
         });
-        res.json({ data: deconstructAnswer(resp.data) });
+        res.json({ data: deconstructAnswer(resp.data, mapper) });
     } catch (error) {
         if (error.response) {
             const { errorMessage, statusCode } = parseErrorSwoogo(error);
@@ -62,13 +71,13 @@ const post = async (req, res = response) => {
 const getOne = async (req, res = response) => {
     const { registrantId } = req.params;
     const body = req.body;
-    const formData = convertJson2Form(body);
+    const formData = convertJson2Form(body, mapper);
     formData.append('event_id', req.integration.event_id);
     try {
         const resp = await axios.get(`${process.env.SWOOGO_APIURL}registrants/${registrantId}.json`, {
             headers: { "Authorization": "Bearer " + await authentication() }
         });
-        res.json({ data: deconstructAnswer(resp.data) });
+        res.json({ data: deconstructAnswer(resp.data, mapper) });
     } catch (error) {
         if (error.response) {
             const { errorMessage, statusCode } = parseErrorSwoogo(error);
@@ -86,13 +95,13 @@ const getOne = async (req, res = response) => {
 const put = async (req, res = response) => {
     const { registrantId } = req.params;
     const body = req.body;
-    const formData = convertJson2Form(body);
+    const formData = convertJson2Form(body, mapper);
     formData.append('event_id', req.integration.event_id);
     try {
         const resp = await axios.put(`${process.env.SWOOGO_APIURL}registrants/update/${registrantId}.json`, formData, {
             headers: { "Authorization": "Bearer " + await authentication() }
         });
-        res.json({ data: deconstructAnswer(resp.data) });
+        res.json({ data: deconstructAnswer(resp.data, mapper) });
     } catch (error) {
         if (error.response) {
             const { errorMessage, statusCode } = parseErrorSwoogo(error);
@@ -115,7 +124,7 @@ const remove = async (req, res = response) => {
         const resp = await axios.put(`${process.env.SWOOGO_APIURL}registrants/update/${registrantId}.json`, formData, {
             headers: { "Authorization": "Bearer " + await authentication() }
         });
-        res.json({ data: deconstructAnswer(resp.data) });
+        res.json({ data: deconstructAnswer(resp.data, mapper) });
     } catch (error) {
         if (error.response) {
             const { errorMessage, statusCode } = parseErrorSwoogo(error);
@@ -128,72 +137,6 @@ const remove = async (req, res = response) => {
             });
         }
     }
-}
-
-const deconstructAnswer = (answer) => {
-    return {
-        first_name         : answer.first_name,
-        last_name          : answer.last_name,
-        email              : answer.email,
-        id                 : answer.id,
-        mobile_phone       : answer.mobile_phone,
-        job                : answer.job_title,
-        company            : answer.company,
-        registration_status: answer.registration_status,
-        external_id        : getValueOrNull(answer[mapper.external_id]),
-        assistant_type     : getValueOrNull(answer[mapper.assistant_type]),
-        country            : getValueOrNull(answer[mapper.country]),
-        company_description: getValueOrNull(answer[mapper.company_description]),
-        company_logo       : getValueOrNull(answer[mapper.company_logo]),
-        company_web        : getValueOrNull(answer[mapper.company_web])
-    }
-}
-
-const convertJson2Form = (body) => {
-    const formData = new FormData();
-    for (const key in body) {
-        if (Object.hasOwnProperty.call(body, key)) {
-            const value = body[key];
-            if (typeof value === 'object' && value !== null) {
-                formData.append(key, JSON.stringify(value));
-            } else {
-                if (val = getValueByKey(key)) {
-                    formData.append(val, value);
-                } else {
-                    formData.append(key, value);
-                }
-            }
-        }
-    }
-    return formData
-}
-
-const parseErrorSwoogo = (error) => {
-    let errorMessage = error.response.data.message;
-    const statusCode = error.response.status;
-    if (statusCode == 422) {
-        errorMessage = error.response.data[0].message
-    }
-    return { errorMessage, statusCode }
-}
-
-const getKeyByValue = (value) => {
-    for (let key in mapper) {
-        if (mapper.hasOwnProperty(key)) {
-            if (mapper[key] === value) {
-                return key;
-            }
-        }
-    }
-    return null;
-}
-
-const getValueByKey = (key) => {
-    return mapper.hasOwnProperty(key) ? mapper[key] : null;
-}
-
-const getValueOrNull = (value) => {
-    return (value) ? value : ""
 }
 
 module.exports = {
--- helpers/utils.js
+++ helpers/utils.js
@@ -27,9 +27,78 @@ const buildError = (path, msg, value) => {
     }
 }
 
+const getValueByKey = (key, object) => {
+    return object.hasOwnProperty(key) ? object[key] : null;
+}
+
+const getValueOrNull = (value) => {
+    return (value) ? value : ""
+}
+
+const parseErrorSwoogo = (error) => {
+    let errorMessage = error.response.data.message;
+    const statusCode = error.response.status;
+    if (statusCode == 422) {
+        errorMessage = error.response.data[0].message
+    }
+    return { errorMessage, statusCode }
+}
+
+const convertJson2Form = (body, objectMap) => {
+    const formData = new FormData();
+    for (const key in body) {
+        if (Object.hasOwnProperty.call(body, key)) {
+            const value = body[key];
+            if (typeof value === 'object' && value !== null) {
+                formData.append(key, JSON.stringify(value));
+            } else {
+                if (val = getValueByKey(key, objectMap)) {
+                    formData.append(val, value);
+                } else {
+                    formData.append(key, value);
+                }
+            }
+        }
+    }
+    return formData
+}
+
+const getString = (object) => {
+    var values = [];
+    for (var clave in object) {
+        if (object.hasOwnProperty(clave)) {
+            values.push(object[clave]);
+        }
+    }
+    return values.join(',');
+}
+
+const deconstructAnswer = (answer, mapper) => {
+    const result = {};
+    for (const key in mapper) {
+        result[key] = getValueOrNull(answer[mapper[key]]);
+    }
+    return result;
+}
+
+const getAllFields = (items, mapper) => {
+    const results = [];
+    items.forEach(function (item) {
+        results.push(deconstructAnswer(item, mapper));
+    });
+    return results;
+}
+
 module.exports = {
     calcPage,
     integrationTypes,
     encryptPassword,
-    buildError
+    buildError,
+    getValueByKey,
+    getValueOrNull,
+    parseErrorSwoogo,
+    convertJson2Form,
+    getString,
+    deconstructAnswer,
+    getAllFields
 }
