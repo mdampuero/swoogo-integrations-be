280fb3f2ec0dd9f8b3c91a0ff2d3d7232a565856
Mauricio Ampuero <mdampuero@gmail.com>
2024-03-12 11:17:35 +0100

    Test demo vars

--- controllers/demos.controllers.js
+++ controllers/demos.controllers.js
@@ -2,46 +2,31 @@ const { response, request } = require("express");
 const Demo = require("../models/demo");
 const { demoQuery } = require('../helpers/demo');
 const { calcPage } = require('../helpers/utils');
-const nodemailer = require('nodemailer');
+
 const demosGet = async (req = request, res = response) => {
-    var transporter = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-            user: "a4140ccebccb5f",
-            pass: "ad647ff494915d"
-        }
-    });
-    const mailOptions = {
-        from: 'tu_correo@gmail.com',
-        to: 'destinatario@example.com',
-        subject: 'Prueba de envío de correo electrónico con Nodemailer y Node.js',
-        text: 'Este es un correo de prueba enviado con Nodemailer y Node.js'
-    };
+    const schema = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+    const host = process.env.NODE_ENV === 'production' ? process.env.HOST : 'localhost';
+    const port = process.env.NODE_ENV === 'production' ? '' : ':' + process.env.PORT;
 
-    // Enviar el correo electrónico
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.error('Error al enviar el correo electrónico:', error);
-        } else {
-            console.log('Correo electrónico enviado correctamente:', info.response);
-        }
-    });
-
-    const { limit, sort, direction, offset, query } = demoQuery(req);
-    const [total, result] = await Promise.all([
-        Demo.countDocuments(query),
-        Demo.find(query)
-            .sort([[sort, direction]])
-            .limit(limit)
-            .skip(offset)
-    ])
     res.json({
-        total,
-        pages: calcPage(total, limit),
-        limit,
-        data: result
+        schema,
+        host,
+        port
     })
+    // const { limit, sort, direction, offset, query } = demoQuery(req);
+    // const [total, result] = await Promise.all([
+    //     Demo.countDocuments(query),
+    //     Demo.find(query)
+    //         .sort([[sort, direction]])
+    //         .limit(limit)
+    //         .skip(offset)
+    // ])
+    // res.json({
+    //     total,
+    //     pages: calcPage(total, limit),
+    //     limit,
+    //     data: result
+    // })
 }
 
 const demosGetOne = async (req, res = response) => {
