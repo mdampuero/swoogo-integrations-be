495fa3049d0b29760529d5783976df32bbd1d258
Mauricio Ampuero <mdampuero@gmail.com>
2024-04-10 21:52:38 +0200

    coso

--- server.js
+++ server.js
@@ -39,9 +39,7 @@ class Server {
         this.app.use(bodyParser.json({ limit: '50mb' }));
         this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
         this.app.use(morgan('dev'))
-        this.app.use(cors({
-            origin: whitelist
-        }))
+        this.app.use(cors())
         this.app.use(express.json())
         this.app.use((req, res, next) => {
             req.io = this.io;
